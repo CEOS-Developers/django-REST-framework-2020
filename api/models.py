5233eaade665d724e8d3dcd73f7a366e9feebbbe
Kim-Ha-Jeong <kha0318@naver.com>
2020-04-10 01:17:44 +0900

    테이블 전체 재생성

--- api/models.py
+++ api/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 
-# 모델링 과제를 이곳에서 해주시면 됩니다! (주석은 나중에 지우셔도 돼요!)
-
 class Member(models.Model):
     email = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
@@ -14,29 +12,43 @@ class Member(models.Model):
 
 
 class Branch(models.Model):
+    branch_name = models.CharField(max_length=20, primary_key=True)
     location = models.CharField(max_length=50)
     total_theater = models.IntegerField()
 
     def __str__(self):
-        return self.id
+        return self.branch_name
 
 
 class Theater(models.Model):
+    theater_no = models.CharField(max_length=10, primary_key=True)
+    branch = models.ForeignKey('Branch', on_delete=models.CASCADE)
     total_seat = models.IntegerField()
 
     def __str__(self):
-        return self.id
+        return self.theater_no
 
 
 class Seat(models.Model):
+    seat_no = models.IntegerField(primary_key=True)
     theater = models.ForeignKey('Theater', on_delete=models.CASCADE)
+    branch = models.ForeignKey('Branch', on_delete=models.CASCADE)
     is_reservation = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.id
+        return self.seat_no
+
+
+class Movie(models.Model):
+    movie_title = models.CharField(max_length=50)
+    genre = models.CharField(max_length=20)
+
+    def __str__(self):
+        self.id
 
 
 class Schedule(models.Model):
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
     theater = models.ForeignKey('Theater', on_delete=models.CASCADE)
     branch = models.ForeignKey('Branch', on_delete=models.CASCADE)
     schedule_date = models.DateField()
@@ -44,15 +56,6 @@ class Schedule(models.Model):
 
     def __str__(self):
         return self.id
-
-
-class Movie(models.Model):
-    schedule = models.ForeignKey('Schedule', on_delete=models.CASCADE)
-    movie_title = models.CharField(max_length=50)
-    genre = models.CharField(max_length=20)
-
-    def __str__(self):
-        self.id
 
 
 class ReservationTicket(models.Model):
@@ -67,7 +70,7 @@ class ReservationTicket(models.Model):
 
 
 class Pay(models.Model):
-    reservation = models.ForeignKey('ReservationTicket', on_delete=models.CASCADE,related_name='reservation_id')
+    reservation = models.ForeignKey('ReservationTicket', on_delete=models.CASCADE)
     member = models.OneToOneField(
         'Member',
         on_delete=models.CASCADE
@@ -75,3 +78,7 @@ class Pay(models.Model):
     payment_date = models.DateField()
     price = models.IntegerField()
     payment_option = models.CharField(max_length=20)
+
+    def __str__(self):
+        self.id
+
