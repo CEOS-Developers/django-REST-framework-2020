a369f635d99a70b59af1480262b434a32f4cfda2
KylieBaek <59535282+KylieBaek@users.noreply.github.com>
2020-04-12 16:41:24 +0900

    first modelling

--- api/models.py
+++ api/models.py
@@ -1,8 +1,54 @@
 from django.db import models
 
-# 모델링 과제를 이곳에서 해주시면 됩니다! (주석은 나중에 지우셔도 돼요!)
-
 # [제약조건]
 # 1. 1:1과 1:n의 관계 포함
 # 2. 각 모델에 필드 최소 3개 이상 포함
 # 3. 서비스 관련 모델 3개 이상 + 유저 모델 1개 구현 (단, 유저는 필수 아님)
+
+
+# 하나의 영화관(db : movies)에서 상영되는 영화, 누적관객 수, 상영날짜 등으로 모델링
+
+class Movie(models.Model):
+    title = models.TextField(primary_key=True)
+    director = models.CharField(max_length=200)
+    release_year = models.IntegerField()
+    running_time = models.IntegerField()  # minutes 단위
+    country = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class ScreeningDates(models.Model):  # 시기별 누적 관객수
+    title = models.ForeignKey('Movie', on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    finished_date = models.DateTimeField(null=True)
+    total_audience = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class GenreStat(models.Model): # 영화 장르별 통계
+    genre = models.ForeignKey('Movie', on_delete=models.SET_NULL)
+    genre_count =  models.IntegerField()
+
+    def __str__(self):
+        return self.genre
+
+class CountryStat(models.Model): #영화 국가별 통계
+    country = models.ForeignKey('Movie', on_delete=models.SET_NULL)
+    country_count = models.IntegerField()
+
+    def __str__(self):
+        return self.country
+
+class Workers(models.Model):  # 영화관 직원
+    name = models.CharField(max_length=100)
+    birth = models.DateTimeField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length=100)
+    position = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
