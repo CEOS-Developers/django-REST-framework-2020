e644426ed42d77e6e0f9d665fed7d67e27859db2
jystella17 <jystella21617@gmail.com>
2020-04-17 19:03:26 +0900

    models.py feedback

--- api/models.py
+++ api/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 
 class Major(models.Model):
     def __str__(self):
         return self.major_name
-    major_num = models.IntegerField(default=0, primary_key=True)
-    major_name = models.CharField(max_length=200)
+
+    num = models.IntegerField(default=0, primary_key=True)
+    name = models.CharField(max_length=200)
 
 
 class Student(models.Model):
-    std_id = models.IntegerField(default=0, primary_key=True)
-    std_major = models.ForeignKey(Major, on_delete=models.CASCADE)
+    std_id = models.BigIntegerField(primary_key=True)
+    std_major = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="major")
     grade = models.IntegerField(default=1)
     credits_available = models.IntegerField(default=18)
 
@@ -19,18 +21,21 @@ class Student(models.Model):
 class Professor(models.Model):
     def __str__(self):
         return self.prof_name
+
     prof_id = models.IntegerField(default=0, primary_key=True)
     prof_name = models.CharField(max_length=200)
     prof_major = models.ForeignKey(Major, on_delete=models.CASCADE)
 
 
 class Course(models.Model):
-    course_num = models.IntegerField(default=0, primary_key=True)
-    course_name = models.CharField(max_length=200)
+    num = models.IntegerField(default=0, primary_key=True)
+    name = models.CharField(max_length=200)
     credit = models.IntegerField(default=0)
     prof_name = models.ForeignKey(Professor, on_delete=models.CASCADE)
     classroom = models.CharField(max_length=200)
-    timetable = models.CharField(max_length=200)  # 강의 시간
+    weekday = models.CharField(max_length=100)  # 강의 요일
+    start_time = models.TimeField(auto_now=False, auto_now_add=False)  # 강의시간
+    finish_time = models.TimeField(auto_now=False, auto_now_add=False)
 
 
 class Basket(models.Model):
@@ -40,12 +45,6 @@ class Basket(models.Model):
 
 class Registration(models.Model):
     course_num = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="reg_crs")
-    course_credit = models.ForeignKey(Course, on_delete=models.CASCADE, db_column="credit", related_name="reg_crd")
-    std_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     current_credits = models.IntegerField(default=0)
-    max_credits = models.ForeignKey(Student, on_delete=models.CASCADE, db_column="credits_available",
-                                    related_name="max_crd")
-    registration_time = models.DateTimeField(auto_now=True)
-
-
-
+    succeed_at = models.DateTimeField(auto_now=True)
