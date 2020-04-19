6623fce5e8c9a2464726228fdf931370c5f1fe94
jystella17 <jystella21617@gmail.com>
2020-04-19 15:44:56 +0900

    serializer

---
+++ api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from api.models import Major, Student, Professor, Course, Registration
+
+
+class MajorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Major
+        fields = '__all__'
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['std_id', 'credits_available']
+
+
+class ProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Professor
+        fields = ['prof_name']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Registration
+        fields = '__all__'
