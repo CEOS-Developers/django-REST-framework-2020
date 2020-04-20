c2a85b317b0f80ac79cbd39363880a8ba7a1d710
jystella17 <jystella21617@gmail.com>
2020-04-21 04:15:49 +0900

    models.py

--- api/models.py
+++ api/models.py
@@ -4,17 +4,19 @@ import datetime
 
 
 class Major(models.Model):
-
     def __str__(self):
-        return self.major_name
+        return self.name
 
     num = models.IntegerField(default=0, primary_key=True)
     name = models.CharField(max_length=200)
 
 
 class Student(models.Model):
-    std_id = models.BigIntegerField(primary_key=True)
-    std_major = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="major")
+    def __int__(self):
+        return self.std_id
+
+    std_id = models.BigIntegerField(default=0, primary_key=True)
+    major = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="major")
     grade = models.IntegerField(default=1)
     credits_available = models.IntegerField(default=18)
 
@@ -23,19 +25,19 @@ class Professor(models.Model):
     def __str__(self):
         return self.prof_name
 
-    prof_id = models.IntegerField(default=0, primary_key=True)
+    prof_id = models.IntegerField(default=0)
     prof_name = models.CharField(max_length=200)
     prof_major = models.ForeignKey(Major, on_delete=models.CASCADE)
 
 
 class Course(models.Model):
-    def __str__(self):
-        return self.num
+    def __int__(self):
+        return self.code
 
-    num = models.IntegerField(default=0, primary_key=True)
+    code = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
     credit = models.IntegerField(default=0)
-    prof_name = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name="courses")
     classroom = models.CharField(max_length=200)
     weekday = models.CharField(max_length=100, default='')  # 강의 요일
     start_time = models.TimeField(auto_now=False, auto_now_add=False, null=True)  # 강의시간
@@ -44,17 +46,17 @@ class Course(models.Model):
 
 class Basket(models.Model):
     def __str__(self):
-        return self.course_num
+        return self.course
 
-    course_num = models.ForeignKey(Course, on_delete=models.CASCADE)
-    std_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 
 class Registration(models.Model):
     def __str__(self):
-        return self.course_num
+        return self.course
 
-    course_num = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="reg_crs")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="registrations")
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     current_credits = models.IntegerField(default=0)
     succeed_at = models.DateTimeField(auto_now=True)
