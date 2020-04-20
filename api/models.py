455be0d5d6230532a86c56aebe44da61598cf1a7
jystella17 <jystella21617@gmail.com>
2020-04-21 04:34:46 +0900

    models.py

--- api/models.py
+++ api/models.py
@@ -37,7 +37,7 @@ class Course(models.Model):
     code = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
     credit = models.IntegerField(default=0)
-    professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name="courses")
+    professor = models.ForeignKey(Professor, null=True, on_delete=models.CASCADE, related_name="courses")
     classroom = models.CharField(max_length=200)
     weekday = models.CharField(max_length=100, default='')  # 강의 요일
     start_time = models.TimeField(auto_now=False, auto_now_add=False, null=True)  # 강의시간
@@ -48,15 +48,15 @@ class Basket(models.Model):
     def __str__(self):
         return self.course
 
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, default=0, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, default=0, on_delete=models.CASCADE)
 
 
 class Registration(models.Model):
     def __str__(self):
         return self.course
 
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="registrations")
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, default=0, on_delete=models.CASCADE, related_name="registrations")
+    student = models.ForeignKey(Student, default=0, on_delete=models.CASCADE)
     current_credits = models.IntegerField(default=0)
     succeed_at = models.DateTimeField(auto_now=True)
