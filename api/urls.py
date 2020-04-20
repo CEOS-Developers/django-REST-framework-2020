6f5409c121c798ec91f44bf38c9f3b10725cf6aa
jystella17 <jystella21617@gmail.com>
2020-04-20 19:39:36 +0900

    urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,15 +3,19 @@ from rest_framework import routers
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
+
+router = routers.DefaultRouter()
+
 urlpatterns = [
     path('major/', views.MajorList.as_view()),
-    path('major/<int:pk>', views.MajorDetail.as_view()),
+    path('major/<int:pk>/', views.MajorDetail.as_view()),
     path('student/', views.StudentList.as_view()),
-    path('student/<int:pk>', views.StudentDetail.as_view()),
+    path('student/<int:pk>/', views.StudentDetail.as_view()),
     path('professor/', views.ProfessorList.as_view()),
-    path('professor/<int:pk>', views.ProfessorDetail.as_view()),
+    path('professor/<int:pk>/', views.ProfessorDetail.as_view()),
     path('course/', views.CourseList.as_view()),
-    path('course/<int:pk>', views.CourseDetail.as_view()),
+    path('course/<int:pk>/', views.CourseDetail.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
