53dd8888ed5c93e6848805c10401fe71c628f6b4
jystella17 <jystella21617@gmail.com>
2020-04-22 15:40:36 +0900

    views.py feedback

--- api/views.py
+++ api/views.py
@@ -2,42 +2,42 @@ from api.serializers import *
 from rest_framework import generics
 
 
-class MajorList(generics.ListCreateAPIView):
+class MajorList(generics.ListAPIView):
     queryset = Major.objects.all()
     serializer_class = MajorSerializer
 
 
-class MajorDetail(generics.RetrieveUpdateDestroyAPIView):
+class MajorDetail(generics.RetrieveAPIView):
     queryset = Major.objects.all()
     serializer_class = MajorSerializer
 
 
-class StudentList(generics.ListCreateAPIView):
+class StudentList(generics.ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 
-class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+class StudentDetail(generics.RetrieveAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 
-class ProfessorList(generics.ListCreateAPIView):
+class ProfessorList(generics.ListAPIView):
     queryset = Professor.objects.all()
     serializer_class = ProfessorSerializer
 
 
-class ProfessorDetail(generics.RetrieveUpdateDestroyAPIView):
+class ProfessorDetail(generics.RetrieveAPIView):
     queryset = Professor.objects.all()
     serializer_class = ProfessorSerializer
 
 
-class CourseList(generics.ListCreateAPIView):
+class CourseList(generics.ListAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 
-class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
+class CourseDetail(generics.RetrieveAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
