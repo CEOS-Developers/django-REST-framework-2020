208d707840c3ff88791ee99fa582106298fc72e8
code4human <tena.kim10@gmail.com>
2020-04-25 00:57:55 +0900

    url basename 변경

--- api/urls.py
+++ api/urls.py
@@ -5,12 +5,14 @@ from .views import MyUserViewSet, ProductViewSet, OrderViewSet, ManufacturerView
 
 
 router = routers.DefaultRouter()
-router.register(r'myusers', MyUserViewSet, basename='MyUser')   # 뷰 등록 prefix = myusers, viewset = MyUserViewSet
-router.register(r'products', ProductViewSet, basename='Product')
-router.register(r'orders', OrderViewSet, basename='Order')
-router.register(r'manufacturers', ManufacturerViewSet, basename='Manufacturer')
-router.register(r'delivery', DeliveryViewSet, basename='Delivery')
-router.register(r'reviews', ReviewViewSet, basename='Review')
+# router.register(prefix, viewset, basename)
+# The basename argument is used to specify the initial part of the view name pattern.
+router.register(r'myusers', MyUserViewSet, basename='myuser-detail')
+router.register(r'products', ProductViewSet, basename='product-detail')
+router.register(r'orders', OrderViewSet, basename='order-detail')
+router.register(r'manufacturers', ManufacturerViewSet, basename='manufacturer-detail')
+router.register(r'delivery', DeliveryViewSet, basename='delivery-detail')
+router.register(r'reviews', ReviewViewSet, basename='review-detail')
 
 urlpatterns = [
     path('', include(router.urls)),
