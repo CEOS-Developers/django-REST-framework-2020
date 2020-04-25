9716051652c7f97b90ad3f3db9aa234c687c9041
code4human <tena.kim10@gmail.com>
2020-04-25 23:02:45 +0900

    queryset 함수명 변경

--- api/views.py
+++ api/views.py
@@ -2,9 +2,9 @@
 DRF 는 자주 사용하는 공통적인 view 로직을 그룹화 한 viewsets 를 제공한다.
 여러 개의 view 를 작성하지 않고, 공통적인 행위들을 ViewSet 에 하나로 그룹화하여 간결하게 사용할 수 있다.
 """
-# from django.shortcuts import render
 from django.db.models import QuerySet
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from .models import MyUser, Product, Order, Manufacturer, Delivery, Review
 from .serializers import MyUserSerializer, ProductSerializer, OrderSerializer, \
     ManufacturerSerializer, DeliverySerializer, ReviewSerializer
@@ -22,7 +22,7 @@ class MyUserViewSet(viewsets.ModelViewSet):
         serializer.save()
     """
     @property
-    def data(self):
+    def queryset(self):
         queryset: QuerySet[MyUser]   # type annotation
         queryset = MyUser.objects.all().order_by("-date_joined")
         return queryset
@@ -35,7 +35,7 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
 
     @property
-    def data(self):
+    def queryset(self):
         queryset: QuerySet[Product]
         queryset = Product.objects.all().order_by("-supply_date")
         return queryset
@@ -48,7 +48,7 @@ class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
 
     @property
-    def data(self):
+    def queryset(self):
         queryset: QuerySet[Order]
         queryset = Order.objects.all().order_by("-date_ordered")
         return queryset
@@ -61,7 +61,7 @@ class ManufacturerViewSet(viewsets.ModelViewSet):
     serializer_class = ManufacturerSerializer
 
     @property
-    def data(self):
+    def queryset(self):
         queryset: QuerySet[Manufacturer]
         queryset = Manufacturer.objects.all().order_by("-manu_num")
         return queryset
@@ -74,9 +74,9 @@ class DeliveryViewSet(viewsets.ModelViewSet):
     serializer_class = DeliverySerializer
 
     @property
-    def data(self):
+    def queryset(self):
         queryset: QuerySet[Delivery]
-        queryset = Manufacturer.objects.all().order_by("-transport")
+        queryset = Delivery.objects.all().order_by("-transport")
         return queryset
 
     def perform_create(self, serializer):
@@ -87,7 +87,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
 
     @property
-    def data(self):
+    def queryset(self):
         queryset: QuerySet[Review]
         queryset = Review.objects.all().order_by("-review_num")
         return queryset
