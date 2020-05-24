8b0aa78dad344d1ea29798a4891bb5651fa780dd
jystella17 <jystella21617@gmail.com>
2020-05-24 22:10:14 +0900

    models.py

--- api/models.py
+++ api/models.py
@@ -1,6 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
-import datetime
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    def __int__(self):
+        return self.user_id
+    user_id = models.IntegerField(null=False, default=0, unique=True)
+    name = models.CharField(null=False, default='', max_length=100)
+    password = models.CharField(max_length=200)
+    email = models.EmailField(max_length=255, unique=True)
 
 
 class Major(models.Model):
@@ -15,10 +23,11 @@ class Student(models.Model):
     def __int__(self):
         return self.std_id
 
-    std_id = models.BigIntegerField(default=0, primary_key=True)
+    std_id = models.ForeignKey(User, on_delete=models.CASCADE, unique=True, primary_key=True, related_name="std_id")
     major = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="major")
     grade = models.IntegerField(default=1)
     credits_available = models.IntegerField(default=18)
+    current_credits = models.IntegerField(default=0)
 
 
 class Professor(models.Model):
@@ -34,18 +43,25 @@ class Course(models.Model):
     def __int__(self):
         return self.code
 
-    code = models.IntegerField(default=0)
+    code = models.IntegerField(default=0, unique=True)
     name = models.CharField(max_length=200)
     credit = models.IntegerField(default=0)
-    professor = models.ForeignKey(Professor, null=True, on_delete=models.CASCADE, related_name="courses")
+    professor = models.ForeignKey(Professor, null=True, on_delete=models.CASCADE, related_name="prof")
     classroom = models.CharField(max_length=200)
-    weekday = models.CharField(max_length=100, default='')  # 강의 요일
+    WEEKDAY_CHOICE = [
+        ('Mon', 'Monday'),
+        ('Tue', 'Tuesday'),
+        ('Wed', 'Wednesday'),
+        ('Thu', 'Thursday'),
+        ('Fri', 'Friday')
+    ]
+    weekday = models.CharField(max_length=20, choices=WEEKDAY_CHOICE)  # 강의 요일
     start_time = models.TimeField(auto_now=False, auto_now_add=False, null=True)  # 강의시간
     finish_time = models.TimeField(auto_now=False, auto_now_add=False, null=True)
 
 
 class Basket(models.Model):
-    def __str__(self):
+    def __int__(self):
         return self.course
 
     course = models.ForeignKey(Course, null=True, on_delete=models.CASCADE)
@@ -53,10 +69,9 @@ class Basket(models.Model):
 
 
 class Registration(models.Model):
-    def __str__(self):
+    def __int__(self):
         return self.course
 
     course = models.ForeignKey(Course, null=True, on_delete=models.CASCADE, related_name="registrations")
     student = models.ForeignKey(Student, null=True, on_delete=models.CASCADE)
-    current_credits = models.IntegerField(default=0)
     succeed_at = models.DateTimeField(auto_now=True)
