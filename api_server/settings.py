04b51b7f877ea2a765cf86f150dd71ac405a88e0
jhp2060 <jhp2060@gmail.com>
2020-05-27 02:36:03 +0900

    database setting reverted

--- api_server/settings.py
+++ api_server/settings.py
@@ -111,13 +111,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-# DATABASES = secrets['DB_SETTINGS']
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = secrets['DB_SETTINGS']
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
